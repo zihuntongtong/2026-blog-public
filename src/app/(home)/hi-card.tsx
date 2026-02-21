fce504e502ad2ca569c7b9b10744647a0d34908a
zihuntongtong <zihuntongtong@gmail.com>
2026-02-22 06:59:15 +0800

    Update hi-card.tsx

--- src/app/(home)/hi-card.tsx
+++ src/app/(home)/hi-card.tsx
@@ -48,7 +48,7 @@ export default function HiCard() {
 				)}
 				<img src='/images/avatar.png' className='mx-auto rounded-full' style={{ width: 120, height: 120, boxShadow: ' 0 16px 32px -5px #E2D9CE' }} />
 				<h1 className='font-averia mt-3 text-2xl'>
-					{greeting} <br /> I'm <span className='text-linear text-[32px]'>{username}</span> , Nice to <br /> meet you!
+					{greeting} <br /> 我是 <span className='text-linear text-[32px]'>{username}</span> , <br />  幸会!
 				</h1>
 			</Card>
 		</HomeDraggableLayer>
